1e6b4480462626e8c25ea9c5a249fdbf4c00b9e1
Ting-Kim <taeho916@gmail.com>
2020-10-02 05:30:44 +0900

    solve greedy problem
    3109 from acmicpc

--- greedy/n3109.py
+++ greedy/n3109.py
@@ -1,58 +1,61 @@
 # / - \ 순으로 탐색
 # 점마다 /-\ 순으로 반복해서 진행하며, 진행 성공한 경우 o체크 + 다음 process로..
-def search(graph, x, y):
-    global answer
-    if x < 0 or x >= len(graph) or y < 0 or y >= len(graph[0])\
-            or graph[x][y] != '.':
-        return
+def search(x, y):
 
-    # print(graph)
-
-    # graph[x][y] = 'o'   # 방문 체크
-    if y == len(graph[0])-1 and graph[x][y] == '.':
+    if y == m-1:
         graph[x][y] = 'o'   # 방문 체크
-        answer += 1
-        return
-
+        # answer += 1
+        return True
 
     for i in range(3):
         nx = x + dx[i]
         ny = y + dy[i]
-        if nx >= 0 and nx < len(graph) and ny >= 0 and ny < len(graph[0]) and graph[nx][ny] == '.':
-            graph[x][y] = 'o'   # 방문 체크
-            search(graph, nx, ny)
-            break
-    return
+
+        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == '.':
+            graph[nx][ny] = 'o'  # 방문 체크
+
+            if search(nx, ny):
+                return True
+
+    return False
 
 
-def install(graph, a, b):
-    global answer
+import sys
 
-    for i in range(a):
-        search(graph, i, 0)
-
-    return answer
-
-
-
-n, m = map(int, input().split())
-graph = []
+n, m = map(int, sys.stdin.readline().strip().split())
 dx = [-1, 0, 1]
 dy = [1, 1, 1]
 answer = 0
+graph = [list(sys.stdin.readline().strip()) for _ in range(n)]
+# graph = []
+# for _ in range(n):
+#     graph.append(list(sys.stdin.readline().strip()))
 
-for _ in range(n):
-    graph.append(list(input()))
+for i in range(n):
+    if search(i, 0):
+        answer += 1
+# install(graph, n)
 
-answer = install(graph, n, m)
 
-# for i in range(n):
-#     print(graph[i])
+for i in range(n):
+    print(graph[i])
+
 print(answer)
 '''
-5 5
-.x...
-..xxxxxxxx
-xxxxx
-xxxxx
+15 15
+.xxxxxxxxxx....
+...x.......xxx.
+...x.......x...
+..xx.......xx..
+...x........xx.
+.x.x......x.x..
+...x......xx...
+.x.x....xxx....
+.x....x.x......
+.x.....xx.x....
+.x..x.xx.......
+.....xx........
+....x..........
+......x........
+...............
 '''
--- test.py
+++ test.py
@@ -1,9 +1,46 @@
-# arr = [i for i in range(30, 0, -1)]
-arr = [i for i in range(30)]
+# / - \ 순으로 탐색
+# 점마다 /-\ 순으로 반복해서 진행하며, 진행 성공한 경우 o체크 + 다음 process로..
+def search(graph, x, y):
+    global answer
+    if x < 0 or x >= len(graph) or y < 0 or y >= len(graph[0])\
+            or graph[x][y] != '.':
+        return False
 
-a = 10
-b = 20
-print(arr)
-print(arr[a])
-print(arr[b])
-print(arr[a:b])
+    if y == len(graph[0])-1 and graph[x][y] == '.':
+        graph[x][y] = 'o'   # 방문 체크
+        answer += 1
+        return True
+
+
+    for i in range(3):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if search(graph, nx, ny):
+            graph[x][y] = 'o'   # 방문 체크
+            return True
+    return False
+
+def install(graph, a, b):
+    global answer
+
+    for i in range(a):
+        search(graph, i, 0)
+
+    return answer
+
+
+
+n, m = map(int, input().split())
+graph = []
+dx = [-1, 0, 1]
+dy = [1, 1, 1]
+answer = 0
+
+for _ in range(n):
+    graph.append(list(input()))
+
+answer = install(graph, n, m)
+
+print(answer)
+

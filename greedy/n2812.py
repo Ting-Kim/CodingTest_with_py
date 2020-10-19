b3ac4af89aa2f4b46e33a36ebb3a5bf618fd1365
Ting-Kim <taeho916@gmail.com>
2020-10-19 23:05:09 +0900

    Update n2812.py
    have to retry..

--- greedy/n2812.py
+++ greedy/n2812.py
@@ -1,30 +1,28 @@
 
 # 앞에서부터 k+1자리까지의 숫자중 가장 큰 숫자 남기고 삭제
 # 그 뒷수자부터도 똑같이 반복
+# 재귀.. 메모리 초과
 
-def delete(input, count, i):
+
+def delete(input, count):
+    answer = ''
     if count < 1:
-        return input
-    a = input[:count+1]
-    index = a.index(max(a))
-    if index < i:
-        a = input[i:count+i+1]
-        index = a.index(max(a))
+        return answer
 
-    answer += 1
+    # a = input[:count+1]
+    MNum = max(input[:count+1])
+    index = input[:count+1].index(MNum)
+
     count -= (index)
-    print(index)
-    print(count)
-    input = input[index:]
-    i += 1
+    # print(count)
+    # print(index)
+    answer += MNum
+    input = input[index+1:]
 
-
-    return delete(input, count, i)
+    return delete(input, count, answer)
 
 n, k = map(int, input().split())
 num = input()
-score = 0
-ahswer = ''
-delete(num, k, score)
+delete(num, k)
 
 print(answer)

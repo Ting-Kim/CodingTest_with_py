e11155ba6ea52e8c14e0486d558a149b283d2ddd
Ting-Kim <59888684+Ting-Kim@users.noreply.github.com>
2020-11-30 07:42:56 +0900

    Update 1.py

--- programmers/k-digital/1.py
+++ programmers/k-digital/1.py
@@ -1,9 +1,11 @@
 def solution(num):
     n = str(num)
     answer = n
+
+    # 완전 탐색
     while True:
         l = len(answer)
-        if l % 2 != 0:
+        if l % 2 != 0: # 자릿수가 홀수일 때는 자릿수 + 1인 최솟값 반환
             print(answer)
             temp = ['1'] + ['0' for i in range(l)]
             # temp.insert(0, '1')
@@ -16,10 +18,13 @@ def solution(num):
         print(answer)
         mulA = 1
         mulB = 1
+
+        # 맨 앞, 맨 뒤 인덱스를 통해서 두 변수에 각각 곱해준다.
         for i in range(len(answer)//2):
             mulA *= int(answer[i])
             mulB *= int(answer[-(i+1)])
 
+        # 비교
         if mulA == mulB:
             print("mulA = ", mulA)
             print("mulB = ", mulB)
